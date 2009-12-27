5ea0158f1c3dea031439aafc9e4e4644c100301d
me <me@59e52cde-48f0-de11-8f6a-000c2900fe0d>
2009-12-27 03:57:14 +0000

    Added login, logout.
    git-svn-id: svn+ssh://nfsn/home/protected/repo/hive/trunk@3 59e52cde-48f0-de11-8f6a-000c2900fe0d

--- account/urls.py
+++ account/urls.py
@@ -8,3 +8,11 @@ urlpatterns = patterns('hive.account.views',
     # (r'^prefs$', 'preferences'),
 )
 
+urlpatterns += patterns('',
+    (r'^login$', 'django.contrib.auth.views.login',
+        {'template_name': 'account/login.html'}),
+    (r'^logout$', 'django.contrib.auth.views.logout',
+        {'template_name': 'account/logout.html'}),
+
+)
+
