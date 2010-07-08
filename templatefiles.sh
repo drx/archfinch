df2c1e34a49681c3c7e0473a0b05456594dedfd3
Łukasz Zapart <drx@drx.pl>
2010-07-08 05:32:18 +0200

    Added templatefiles.sh.

---
+++ templatefiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# List all template files.
+#
+# Useful for opening the project in an editor like Vim which doesn't have
+#  any functionality for projects. 
+#
+# You might use it like this:
+#
+#  vim -p `./templatefiles.sh`
+find . -regextype posix-extended -regex ".*\.html" | sort
