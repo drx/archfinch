657ee1926cd299e3bda7ed4e469d5887a47e863f
Łukasz Zapart <drx@drx.pl>
2010-07-11 19:32:03 +0200

    Primitive search added.

---
+++ search/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from main.models import Item
+import re
+
+def query(request):
+    q = request.GET['q']
+
+    words = re.findall(r'("[\w ]+"|\w+)', q)
+
+    results = Item.objects.all()
+    for word in words:
+        results = results.filter(name__icontains=word)
+
+    if request.user.is_authenticated():
+        return render_to_response('search/results.html',
+            {'query': q, 'results': results},
+            context_instance=RequestContext(request))
+
+    else:
+        return render_to_response('search/results_anonymous.html',
+            {'query': q, 'results': results},
+            context_instance=RequestContext(request))
