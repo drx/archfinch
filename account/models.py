47ef16ac14c68770997cd63dfe6442c9b8626b48
Łukasz Zapart <drx@drx.pl>
2010-07-19 01:13:17 +0200

    Fixed signup bug which would register users with the django.contrib class instead of the custom subclass.

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from users.models import User
 from django import forms
 
 
