6e10aabd9c7da999b43e7e518bf57a56ec6682d0
Łukasz Zapart <drx@drx.pl>
2010-07-22 05:52:29 +0200

    Improved search results further.

--- search/views.py
+++ search/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.db.models import Count
-from main.models import Item
+from django.db.models import Count, Q
+from main.models import Item, Category
 import re
 
 
@@ -17,12 +17,48 @@ def query(request):
     words = re.findall(r'([\w:]*?"[\w: ]+"[\w:]*?|[\w:]+)', query)
     words = map(lambda w: w.replace('"',''), words)
 
+    modifiers = {}
+
+    words_cleaned = []
+    for word in words:
+        mod, comma, arg = word.partition(':')
+        if not comma:
+            words_cleaned.append(word)
+            continue
+
+        if mod == 'in':
+            modifiers['in'] = arg
+
+        elif mod == 'exact':
+            modifiers['exact'] = arg
+
+        else:
+            words_cleaned.append(word)
+
+    words = words_cleaned
+    title = ' '.join(words)
+
     results = Item.objects.all()
+
+    for mod in modifiers:
+        if mod == 'exact':
+            results = results.filter(name__iexact=modifiers[mod])
+
+        elif mod == 'in':
+            cat_name = modifiers[mod]
+            category = Category.objects.get(
+                Q(name__iexact=cat_name)|Q(element_singular__iexact=cat_name)|Q(element_plural__iexact=cat_name)
+            )
+            results = results.filter(category=category)
+
     for word in words:
         results = results.filter(name__icontains=word)
 
     count = results.count()
-    results = results.annotate(Count('opinion')).order_by('-opinion__count', 'name')[start:start+n]
+    results = results.annotate(Count('opinion')).extra(
+        select={'is_exact': "name ILIKE %s"},
+        select_params=(title,)
+        ).order_by('-is_exact', '-opinion__count', 'name')[start:start+n]
 
     left = count-(start+n)
 
