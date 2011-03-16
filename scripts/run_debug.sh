da3a513e646c33624d8528ade641bfed27da1851
Łukasz Zapart <drx@drx.pl>
2011-03-16 17:58:58 +0100

    Added restart to run_debug.sh

--- scripts/run_debug.sh
+++ scripts/run_debug.sh
@@ -2,24 +2,36 @@ killa ()
 {
     ps ax | grep $* | awk '{print $1}' | xargs kill 2>/dev/null
 }
-
+start ()
+{
+    python manage.py runserver 0.0.0.0:8000&
+    /var/rabbitmq_server-2.2.0/sbin/rabbitmq-server&
+    python manage.py celeryd&
+    memcached&
+    #sudo searchd&
+}
+stop ()
+{
+    killa celeryd
+    /var/rabbitmq_server-2.2.0/sbin/rabbitmqctl -q stop
+    killa runserver
+    killall memcached
+    #killall searchd
+}
 case "$1" in
     start)
-        python manage.py runserver 0.0.0.0:8000&
-        /var/rabbitmq_server-2.2.0/sbin/rabbitmq-server&
-        python manage.py celeryd&
-        memcached&
-        #sudo searchd&
+        start
     ;;
     stop)
-        killa celeryd
-        /var/rabbitmq_server-2.2.0/sbin/rabbitmqctl -q stop
-        killa runserver
-        killall memcached
-        #killall searchd
+        stop
+    ;;
+    restart)
+        stop
+        sleep 1
+        start        
     ;;
     *)
-        echo $0 "{start|stop}"
+        echo $0 "{start|stop|restart}"
     ;;
 esac
 
