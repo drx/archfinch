434cd6fef1f11230119f73c45662500a3edecb7d
Luke Zapart <drx@drx.pl>
2011-04-21 17:54:45 +0200

    Ported wilson.sql to plpgsql (plpython is untrusted fffff)

--- main/sql/wilson.sql
+++ main/sql/wilson.sql
@@ -1,21 +1,28 @@
-CREATE OR REPLACE FUNCTION wilson_score(item_id integer) RETURNS integer
+CREATE OR REPLACE FUNCTION wilson_score(item_id integer) RETURNS real
     AS $$
-    from math import sqrt
-    z = 2
-    extremity_factor = 3
+    DECLARE 
+        z CONSTANT real := 2;
+        extremity_factor CONSTANT integer := 3;
+        positive integer := 0;
+        negative integer := 0;
+        total integer := 0;
+        phat real;
+        rating_row record;
+    BEGIN
+    FOR rating_row in SELECT rating, count(1) as count from main_opinion where item_id=$1 group by rating
+    LOOP
+        CASE rating_row.rating
+            WHEN 5 THEN positive := positive + rating_row.count*extremity_factor;
+            WHEN 4 THEN positive := positive + rating_row.count;
+            WHEN 2 THEN negative := negative + rating_row.count;
+            WHEN 1 THEN negative := negative + rating_row.count*extremity_factor;
+            ELSE NULL;
+        END CASE;
+    END LOOP;
+    total := positive + negative;
+    if total = 0 then return 0; end if;
 
-    ratings_q = plpy.execute("SELECT rating, count(1) as count from main_opinion group by rating having item_id=$1 order by rating asc", [item_id])
-    ratings = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
-    for rating in ratings_q:
-        ratings[rating['rating']+1] = rating['count']
-    positive = ratings[4] + ratings[5]*extremity_factor
-    negative = ratings[2] + ratings[1]*extremity_factor
-    total = positive+negative
-    if total == 0: 
-        return 0
-        
-    observation = float(positive)/total
-    
-    return (observation + z*z/(2*total) - z*sqrt((observation*(1-observation)+z*z/(4*total))/total))/(1+z*z/total)    
-    
-    $$ LANGUAGE plpythonu;
+    phat := positive::real/total::real;
+    return (phat + z*z/(2*total) - z*sqrt((phat*(1-phat)+z*z/(4*total))/total))/(1+z*z/total);
+    END;
+    $$ LANGUAGE plpgsql;
