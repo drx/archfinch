28f400dea3c4df0211a9ef1f3b558854b979145a
Luke Zapart <drx@drx.pl>
2011-04-23 08:05:52 +0200

    Added gzip middleware.

--- settings.py
+++ settings.py
@@ -48,6 +48,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
