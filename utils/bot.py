661aa9e60332126d2d3c6f57d47a435f7d0832e9
Luke Zapart <luke@archfinch.com>
2011-04-26 16:51:52 -0700

    Catch all bot exceptions.

--- utils/bot.py
+++ utils/bot.py
@@ -7,9 +7,12 @@ class Bot():
         self.port = 40037
 
     def send_message(self, message):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.port))
-        s.send(message)
-        s.close()
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.host, self.port))
+            s.send(message)
+            s.close()
+        except Exception:
+            pass
 
 bot = Bot()
