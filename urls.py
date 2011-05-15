75b43fcc2393b14c055f27bf6b387da6ea943f27
Luke Zapart <drx@drx.pl>
2011-05-16 01:31:23 +0200

    Allow more tag chars.

--- urls.py
+++ urls.py
@@ -43,7 +43,7 @@ urlpatterns = patterns('',
     url(r'tags/(?P<tag_names>[\w\-\/ ]+)/(?P<page>\d+)', 'archfinch.main.views.recommend', name='fresh-tags-paged'),
     url(r'tags/(?P<tag_names>[\w\-\/ ]+)', 'archfinch.main.views.recommend', name='fresh-tags'),
     (r'^addtag/(?P<item_id>[0-9a-z]+)$', 'archfinch.main.views.add_tag'),
-    url(r'^blocktag/(?P<tag_name>[\w \-]+)$', 'archfinch.main.views.block_tag', name='block-tag'),
+    url(r'^blocktag/(?P<tag_name>[^\/]+)$', 'archfinch.main.views.block_tag', name='block-tag'),
 
     url(r'^search$', 'archfinch.search.views.query', {'query': ''}, name='search-base'),
     url(r'^search/(?P<query>.*?)(?:/(?P<page>\d+))?(?P<json>\.json)?(?P<autocomplete>\.autocomplete)?$', 'archfinch.search.views.query', name='search'),
