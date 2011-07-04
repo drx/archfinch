881cfdb82fa26236c0f38e6809d665078f34cf8a
Luke Zapart <drx@drx.pl>
2011-07-04 05:17:22 +0200

    Fix those annoying 'base36 input too large' errors by raising a 404.

--- wiki/urls.py
+++ wiki/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('archfinch.wiki.views',
-    url(r'^edit/item/(?P<item_id>[0-9a-z]+)$', 'edit', name='wiki-edit-item'),
+    url(r'^edit/item/(?P<item_id>[0-9a-z]{,13})$', 'edit', name='wiki-edit-item'),
 )
